4713d5455e427431e4078c7f29ca7d9fdc84687b
Michael Ramirez <45132037+MykeRam@users.noreply.github.com>
2026-06-02 00:49:57 -0400

    update footer credit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
 
   return (
     <footer className="footer">
-      <p className="footer__developer">Developed by Michael Ramirez</p>
+      <p className="footer__developer">Developed by Michael</p>
       <p className="footer__year">{currentYear}</p>
     </footer>
   );
